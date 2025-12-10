477c25c617528cd27bc76399ee2b682176ab874d
alyavirgia05 <alyavirgia05@gmail.com>
2025-12-10 20:12:58 +0700

    Commit No. 1 – add data binding

--- app/src/main/java/com/example/lab_week_13/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_13/MainActivity.kt
@@ -4,14 +4,10 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModel
-import androidx.recyclerview.widget.RecyclerView
 import com.example.lab_week_13.model.Movie
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import com.google.android.material.snackbar.Snackbar
-import kotlinx.coroutines.launch
-import androidx.lifecycle.Lifecycle
+import com.example.lab_week_13.databinding.ActivityMainBinding
+import androidx.databinding.DataBindingUtil
 
 class MainActivity : AppCompatActivity() {
     private val movieAdapter by lazy {
@@ -24,12 +20,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil
+            .setContentView(this, R.layout.activity_main)
 
         supportActionBar?.title = "LAB WEEK 13"
 
-        val recyclerView: RecyclerView = findViewById(R.id.movie_list)
-        recyclerView.adapter = movieAdapter
+        binding.movieList.adapter = movieAdapter
         val movieRepository = (application as MovieApplication).movieRepository
         val movieViewModel = ViewModelProvider(
             this, object : ViewModelProvider.Factory {
@@ -37,33 +33,9 @@ class MainActivity : AppCompatActivity() {
                     return MovieViewModel(movieRepository) as T
                 }
             })[MovieViewModel::class.java]
-        // fetch movies from the API
-        // lifecycleScope is a lifecycle-aware coroutine scope
-        lifecycleScope.launch {
-            // repeatOnLifecycle is a lifecycle-aware coroutine builder
-            // Lifecycle.State.STARTED means that the coroutine will run
-            // when the activity is started
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                launch {
-                    // collect the list of movies from the StateFlow
-                    movieViewModel.popularMovies.collect {
-                        // add the list of movies to the adapter
-                            movies ->
-                        movieAdapter.addMovies(movies)
-                    }
-                }
-                launch {
-                    // collect the error message from the StateFlow
-                    movieViewModel.error.collect { error ->
-                        // if an error occurs, show a Snackbar with the error message
-                        if (error.isNotEmpty()) Snackbar
-                            .make(
-                                recyclerView, error, Snackbar.LENGTH_LONG
-                            ).show()
-                    }
-                }
-            }
-        }
+
+        binding.viewModel = movieViewModel
+        binding.lifecycleOwner = this
     }
 
     private fun openMovieDetails(movie: Movie) {
